c49a4901f1b6db9f0a6a499cc672947fcd823960
Vandson Falcão <vandson.falcao@inbec.com.br>
2022-09-13 11:56:45 -0300

    feat: finished layout home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx',],
+  content: ['./src/**/*.tsx','./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/bg-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 35.94%, #E1D55D 80.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
   },
   plugins: [],
 }
